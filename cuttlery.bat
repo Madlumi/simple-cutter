fcb0d03280c0fcf27e83d57edd4031198803eba7
Madlumi <61489225+Madlumi@users.noreply.github.com>
2021-07-03 05:25:02 +0300

    adding the time math broke stuff because batch, fixed it

--- cuttlery.bat
+++ cuttlery.bat
@@ -11,34 +11,36 @@ setlocal enabledelayedexpansion
 set itt=0
 for /f "tokens=1-2 delims=," %%i  IN (cuts.txt) do (
 	if !itt! gtr 0 (
-		::generate cuts
-		::TIME MATH
-		::TIME MATH
-		::TIME MATH
-		::TIME MATH
-		::TIME MATH
-		::inputs
+		REM generate cuts
+		REM TIME MATH
+		REM TIME MATH
+		REM TIME MATH
+		REM TIME MATH
+		REM TIME MATH
+		REM inputs
 		set a=%%i
 		set b=%%j
-		::separate data
-		set s1=%a:~-2,9999%
-		set m1=%a:~-5,-3%
-		set h1=%a:~-8,-6%
-		set s2=%b:~-2,9999%
-		set m2=%b:~-5,-3%
-		set h2=%b:~-8,-6%
-		::make sure is defined
+		echo !a!
+		echo !b!
+		REM separate data
+		set s1=!a:~-2,9999!
+		set m1=!a:~-5,-3!
+		set h1=!a:~-8,-6!
+		set s2=!b:~-2,9999!
+		set m2=!b:~-5,-3!
+		set h2=!b:~-8,-6!
+		REM make sure is defined
 		IF NOT DEFINED h1 set h1=0
 		IF NOT DEFINED h2 set h2=0
 		IF NOT DEFINED m1 set m1=0
 		IF NOT DEFINED m2 set m2=0
 		IF NOT DEFINED s1 set s1=0
 		IF NOT DEFINED s2 set s2=0
-		::diference
-		set /a s3=%s2%-%s1%
-		set /a m3=%m2%-%m1%
-		set /a h3=%h2%-%h1%
-		::convert negatives to time
+		REM diference
+		set /a s3=!s2!-!s1!
+		set /a m3=!m2!-!m1!
+		set /a h3=!h2!-!h1!
+		REM convert negatives to time
 		if 0 gtr !s3!  (
 			set /a s3=60+!s3!
 			set /a m3-=1
@@ -47,7 +49,7 @@ for /f "tokens=1-2 delims=," %%i  IN (cuts.txt) do (
 			set /a m3=60+!m3!
 			set /a h3-=1
 		)
-		::pad w zeroes
+		REM pad w zeroes
 		if 10 gtr !s3!  (
 			set s3=0!s3!
 		)
@@ -57,22 +59,22 @@ for /f "tokens=1-2 delims=," %%i  IN (cuts.txt) do (
 		if 10 gtr !h3!  (
 			set h3=0!h3!
 		)
-		::construct time code
+		REM construct time code
 		set c=!h3!:!m3!:!s3!
-		
-		::make clip
+		echo !c!
+		REM make clip
 		ffmpeg -i !inp! -ss !a! -t !c! -c copy part_!itt!.mp4
 		
-		::APPEND TO TXT FILE
+		REM APPEND TO TXT FILE
 		if !itt!==1 (echo file 'part_!itt!.mp4'> parts.txt) else (echo file 'part_!itt!.mp4'>> parts.txt)
 	) else (
-	    ::read file name
+	    REM read file name
 		set inp=%%i
-		::set output name
+		REM set output name
 		set out=!inp:~0,-5![edited]!inp:~-5,-1!
 	) 
 	
-	::itterate on itt
+	REM itterate on itt
 	set /A itt=!itt!+1
 )
 ::combine cuts
